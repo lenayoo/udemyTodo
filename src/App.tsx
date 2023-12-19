cd6d35c3c1abcd92233e37c3f3f20e6f136f1ede
lenayoo <lenayoo17@gmail.com>
2023-12-19 23:05:31 +0900

    finish todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
+import { ChangeEvent, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [inputText, setInputText] = useState<string>("");
+  const [todos, setTodos] = useState<string[]>([]);
+
+  const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+  };
+  const submitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    setTodos((prevTodos) => [...prevTodos, inputText]);
+    setInputText("");
+  };
+
+  console.log(todos);
   return (
     <>
       <h1>To do⛵️</h1>
       <div className="box">
-        <form className="todo-form">
-          <input className="add-input" />
-          <button type="button" className="add-btn">
+        <form className="todo-form" onSubmit={submitHandler}>
+          <input
+            className="add-input"
+            onChange={changeHandler}
+            value={inputText}
+          />
+          <button type="submit" className="add-btn">
             add
           </button>
         </form>
+        {todos && todos.map((todo, index) => <div key={index}>{todo}</div>)}
       </div>
     </>
   );
