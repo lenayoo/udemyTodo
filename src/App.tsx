f1c404dd71a396391c67021cfb10b025162fd134
lenayoo <lenayoo17@gmail.com>
2023-12-19 22:27:00 +0900

    finish structure and styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,14 @@ function App() {
   return (
     <>
       <h1>To do⛵️</h1>
-      <div className="box"></div>
+      <div className="box">
+        <form className="todo-form">
+          <input className="add-input" />
+          <button type="button" className="add-btn">
+            add
+          </button>
+        </form>
+      </div>
     </>
   );
 }
